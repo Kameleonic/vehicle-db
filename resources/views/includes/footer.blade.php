52f2f29962ac41155c1b8048a540563f9b60693e
Kameleonic <959media@gmail.com>
2022-06-07 23:41:54 +0100

    Created pulic/assets/ folder for logos

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,17 +3,18 @@
 
 <div class="footer fixed-bottom bg-primary">
 
-<!-- <div id="ftr-border" class="">
+    <!-- <div id="ftr-border" class="">
 <h6 class="ftr-btn">Expand</h6>
 </div> -->
     <div>
         <div class="footer  text-center ">
             <h6 class="footer-text">Copyright © Kameleonic Design</h6>
-            <img src="./storage/assets/logo.png" class="logo"/>
+            <img src="/assets/logo.png" class="logo" />
             <h6 class="footer-text">{{ date('Y') }} - All rights reserved.</h6>
 
         </div>
 
     </div>
 </div>
+
 </html>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<header class="navbar navbar-light justify-content-center bg-primary p-2" >
+<header class="navbar navbar-light justify-content-center bg-primary p-2">
     <div class="d-flex wd-90 justify-content-between">
-    <img src="/storage/assets/logo-brand.png" class="logo-hdr nav-logo"/>
+        <img src="/assets/logo-brand.png" class="logo-hdr nav-logo" />
 
-    <nav class="nav-menu">
-        <ul class="nav">
-            <li>Home</li>
-            <li>Modify</li>
-            <li></li>
-            <li></li>
-        </ul>
-    </nav>
+        <nav class="nav-menu">
+            <ul class="nav">
+                <li>Home</li>
+                <li>Modify</li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
 
-</div>
+    </div>
 </header>
+
 </html>
